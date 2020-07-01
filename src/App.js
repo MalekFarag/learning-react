27fea39277f157aa285e84690989eb3e2254c723
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-07-01 16:40:02 -0400

    displaying message + test components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Hello from './sayhello';
+import Message from './message';
+
+function App(){
+
+
+
+  return(
+    <div>
+      <h1>App component</h1>
+
+      <Message />
+
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Message(){
+    return(
+        <div className='msg'>
+            <h4>Name:</h4>
+            <p>Message:</p>
+            <h5>Likes: </h5>
+        </div>
+    );
+}
+
+export default Message;
